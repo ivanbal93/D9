49a1ba84f5063c905db9cc0623a6ab8d1f779e78
ivanbal93 <ivanbal93@gmail.com>
2023-03-22 21:21:28 +0400

    homewwork

---
+++ portal/app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import PostList, PostDetail, Search, NewPost, PostUpdate, PostDelete, IndexView, SubscribeView
+from django.views.decorators.cache import cache_page
+
+
+urlpatterns = [
+    path('', cache_page(60*1) (PostList.as_view()), name='post_list'),
+    path('<int:pk>', cache_page(60*5) (PostDetail.as_view()), name='post_detail'),
+    path('search', Search.as_view(), name='search'),
+    path('create', NewPost.as_view(), name='create'),
+    path('<int:pk>/update/', PostUpdate.as_view(), name='update'),
+    path('<int:pk>/delete/', PostDelete.as_view(), name='delete'),
+    path('subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path('subscribe/<int:category_id>', SubscribeView.as_view(), name='add_subscriber'),
+    path('celery_test', IndexView.as_view())
+]
